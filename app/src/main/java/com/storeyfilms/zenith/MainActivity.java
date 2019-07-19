576980041c7f025f180423a32b9b47438a05a46a
ITDevStar <itdevstar@hotmail.com>
2019-07-19 10:01:03 +0300

    init project

---
+++ app/src/main/java/com/storeyfilms/zenith/MainActivity.java
@@ -0,0 +1,14 @@
+package com.storeyfilms.zenith;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
